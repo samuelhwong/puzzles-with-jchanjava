eb79fa86c2d4ca2e8e7413d3c99fe4fab8bad198
Samuel Wong <samuel.h.wong@gmail.com>
2022-04-29 23:20:10 -0400

    Update pangram.js

--- pangram.js
+++ pangram.js
@@ -5,7 +5,7 @@
 // Given a string, detect whether or not it is a pangram. Return True if it is, False if not.
 // Ignore numbers and punctuation.
 
-function isPangram(input) {
+function isPangram(sentence) {
     // Define a list of 26 letters.
 
     // Option1
@@ -14,7 +14,9 @@ function isPangram(input) {
     // var checkList = [a,b,c,d,e,f]    // false!
     // var checkList = []    // true!
 
-    // Option2
+    // Option2 - Let's build a checkList of 26 letters. If a letter is true,
+    // it means we've found that letter in the sentence. If a letter is false,
+    // it means we haven't found it yet.
     var checkList = {
         a: false, // not found (not checked off)
         b: false,
@@ -43,69 +45,70 @@ function isPangram(input) {
         y: false,
         z: false
     };
-    // checkList.g = true     // to check something off the list, set it to true.
-    // checkList['g'] = true  // is another way of doing the same thing
 
-    // var letterFromSentence = 'T';
-    // // next iteration, it becomes:
-    // var letterFromSentence = 'h';
-    // // next iteration, it becomes:
-    // var letterFromSentence = 'e';
-    // // next iteration, it becomes:
-    // var letterFromSentence = 'q';
-
-    //  for letterFromSentence = 0 , check if it's on list, 0+1, check if on list, until s
-    //  i=0;i<Count;i++
+    /////////////////////////
+    // MAIN PROGRAM
+    /////////////////////////
+     
+    console.log("Yay, we got a sentence. It is " + sentence.length + " characters long.");
 
     // Create a variable called "i" and set it to 0. For each time we iterate through the loop, increase i by 1. Stop when (i < sentence.length) is no longer true.
     for (var i = 0; i < sentence.length; i++) {
-        var letterFromSentence = sentence[i];  // get the letter at the "i-th" index
-        
-        // If you see a space, a number, or a punctuation, just move on to the next index.
+        var letter = sentence[i];  // get the letter at the "i-th" index
+        console.log("We are at letter: " + letter);
 
+        // If you see a space, a number, or a punctuation, just move on to the next letter.
+        // This is because spaces, numbers, and punctuation doesn't count in a pangram.
+        if (letter === " ") {
+            console.log("  This is a space, so we'll ignore");
+            continue;
+        }
+
+        console.log("   This is not a space, let's continue");
 
         // Check the letter off the checkList.
-
+        checkList[letter.toLowerCase()] = true;
 
     }
+    
+    console.log(`Here is the checklist: ${JSON.stringify(checkList)}`);
+    
+    var arrayOfLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    for (var item = 0; item < arrayOfLetters.length; item++) {
+        console.log("  Checking letter: " + arrayOfLetters[item]);
+        var checkListItem = checkList[arrayOfLetters[item]];
+        if (checkListItem === false) {
+            console.log("  Found a false, so this is not a pangram");
+            return false;
+        } else {
+            console.log("  Found a true, this could be a pangram so let's keep going");
+        }
+    }
+    
+    // If we are here, then we know it's a pangram. Return true.
+    return true;
 }
 
 // INPUT
-let sentence = "The quick brown fox jumps over the lazy dog";   // 43 chars long
-let sentenceLength = sentence.length;
+var sentence = "The quic333333,3781292834671!@#$%_3k brown fox jumps over the lazy dog";
 
 // EXECUTE
-let output = isPangram(sentence);
+var result = isPangram(sentence);
 
 // OUTPUT
-console.log(output);
-
-
-
-
-1. Make a list of the 26 letters (let's call this the checklist)
-2. For each letter in the sentence, do: 
-   a) Go through the checklist and find a match.
-   b) When we have found a match, check it off.
-   c) Repeat with the next letter in the sentence, until there are no more letters in the sentence
-3. If at least one item on the checklist is not checked, then return false (If everything is checked on the checklist, then return true)
-
-
-
-
-var person = {
-    firstName: "Sam",
-    lastName: "Wong",
-    isTall: true,
-    age: 43,
-    address: {
-        city: "Toronto",
-        province: "Ontario"
-    },
-    kids: [ "Esther", "Hannah", "Simon" ]
+if (result === true) {
+    console.log(">>>>>>>> We found a pangram");
+} else {
+    console.log(">>>>>>>> We did not find a pangram");
 }
 
-console.log("Hello, " + person.firstName + " " + person.lastName);   // "Hello, Sam Wong"
-console.log("He lives in " + person.address.city);   // "He lives in Toronto"
-console.log("He has kids named " + person.kids[0] + ", " + person.kids[1] + ", " + person.kids[2]);  // "He has kids named Esther, Hannah, Simon"
+
+
+
+// 1. Make a list of the 26 letters (let's call this the checklist)
+// 2. For each letter in the sentence, do: 
+//    a) Go through the checklist and find a match.
+//    b) When we have found a match, check it off.
+//    c) Repeat with the next letter in the sentence, until there are no more letters in the sentence
+// 3. If at least one item on the checklist is not checked, then return false (If everything is checked on the checklist, then return true)
 
